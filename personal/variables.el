e4b8d2fe3b80055fda1e71746211ba85bb268069
Iain Beeston <iain.beeston@gmail.com>
2015-07-22 09:35:42 +0100

    Made copy and paste work on osx

--- personal/variables.el
+++ personal/variables.el
@@ -1,2 +1,13 @@
+;; enable copy-paste on the terminal
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(setq interprogram-paste-function 'copy-from-osx)
+(setq interprogram-cut-function 'paste-to-osx)
+
 ;; put a space in the border for linum
 (setq linum-format  "%4d ")
