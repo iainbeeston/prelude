f04d88c2eea2ef01c23d508c3bd8420c27497ddb
Iain Beeston <iain.beeston@gmail.com>
2015-07-22 09:16:58 +0100

    Enabled global linum mode

--- personal/functions.el
+++ personal/functions.el
@@ -1,2 +1,5 @@
 ;; disable the menu bar (prelude enables this by default)
 (menu-bar-mode 0)
+
+;; turn on linum automatically
+(global-linum-mode t)
--- personal/variables.el
+++ personal/variables.el
@@ -1,2 +1,5 @@
 ; allow autocomplete navigation from home row
 (setq ac-use-menu-map t)
+
+;; put a space in the border for linum
+(setq linum-format  "%4d ")
