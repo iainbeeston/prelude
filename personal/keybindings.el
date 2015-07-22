f3e2ebba8b141a20ab25756382b100268c97322c
Iain Beeston <iain.beeston@gmail.com>
2015-07-22 09:19:13 +0100

    Switched autocomplete config from ac to company

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -31,3 +31,10 @@
 (define-key isearch-mode-map (kbd "C-h") 'isearch-del-char)
 (define-key isearch-mode-map (kbd "C-f") 'isearch-ring-advance)
 (define-key isearch-mode-map (kbd "C-b") 'isearch-ring-retreat)
+
+;; redefine keys for company mode
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "M-n") nil)
+  (define-key company-active-map (kbd "M-p") nil)
+  (define-key company-active-map (kbd "C-n") #'company-select-next)
+  (define-key company-active-map (kbd "C-p") #'company-select-previous))
