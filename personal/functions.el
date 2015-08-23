2b5a8f4ad15e3af87c7082074dc6842d59f03012
Iain Beeston <iain.beeston@gmail.com>
2015-08-23 18:51:38 +0100

    Tell flycheck to use jscs on javascript in web-mode

--- personal/functions.el
+++ personal/functions.el
@@ -3,3 +3,7 @@
 
 ;; turn on linum automatically
 (global-linum-mode t)
+
+;; tell flycheck that web-mode can use jscs mode
+(with-eval-after-load 'flycheck
+  (flycheck-add-mode 'javascript-jscs 'web-mode))
--- personal/hooks.el
+++ personal/hooks.el
@@ -2,3 +2,11 @@
 (add-hook 'minibuffer-setup-hook
           (lambda ()
             (local-set-key (kbd "C-h") 'delete-backward-char)))
+
+;; tell flycheck to disable jscs in web mode unless it's a jsx file
+(add-hook 'web-mode-hook
+          (lambda ()
+            (message "checking if web mode is opening a jsx file")
+            (unless (string-match "jsx" (file-name-extension buffer-file-name))
+              (message "disabling jscs (not jsx file)")
+              (setq-default flycheck-disabled-checkers '(javascript-jscs)))))
