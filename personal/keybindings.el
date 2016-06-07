621ad541ffeeb1f5d750b6927d3a2299b2b37153
Iain Beeston <iain@thoughtbot.com>
2016-06-07 10:50:26 +0100

    Removed help keybinding
    Doesn't seem to work anyway

--- personal/keybindings.el
+++ personal/keybindings.el
@@ -1,5 +1,4 @@
 ;; better navigation keys
-(global-set-key (kbd "C-?") 'help-command)
 (global-set-key (kbd "M-?") 'mark-paragraph)
 (global-set-key (kbd "C-h") 'delete-backward-char)
 (global-set-key (kbd "M-h") 'backward-kill-word)
