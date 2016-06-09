0bac2ad1f9786fbf97c6a300d1efeb7275640892
Iain Beeston <iain@thoughtbot.com>
2016-06-09 17:42:10 +0100

    disabled smart parens

--- personal/hooks.el
+++ personal/hooks.el
@@ -15,3 +15,8 @@
 (add-hook 'csv-mode-hook
           (lambda ()
             (csv-align-fields nil (point-min) (point-max))))
+
+;; disable smart parens
+(add-hook 'prelude-prog-mode-hook
+          (lambda ()
+            (smartparens-mode -1)) t)
