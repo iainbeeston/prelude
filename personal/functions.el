6049e71bc0446cf0aafe81d87bd319ee1dfd2f79
Iain Beeston <iain.beeston@gmail.com>
2017-05-15 11:13:17 +0100

    Told web-mode to use eslint not jscs
    jscs is deprecated

--- personal/functions.el
+++ personal/functions.el
@@ -1,6 +1,6 @@
 ;; turn on linum automatically
 (global-linum-mode t)
 
-;; tell flycheck that web-mode can use jscs mode
+;; tell flycheck that web-mode can use eslint
 (with-eval-after-load 'flycheck
-  (flycheck-add-mode 'javascript-jscs 'web-mode))
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
