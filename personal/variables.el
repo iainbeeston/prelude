40abddbe4c0862d8668f994dd823a79ea0e0414c
Iain Beeston <iainbeeston@users.noreply.github.com>
2017-05-20 09:34:07 +0100

    Removed workaround to get kill ring to work with mac clipboard
    Using emacs for os x now so this shouldn't be needed

--- personal/variables.el
+++ personal/variables.el
@@ -1,15 +1,3 @@
-;; enable copy-paste on the terminal
-(defun copy-from-osx ()
-  (shell-command-to-string "pbpaste"))
-(defun paste-to-osx (text &optional push)
-  (let ((process-connection-type nil))
-    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
-      (process-send-string proc text)
-      (process-send-eof proc))))
-(when (eq system-type 'darwin)
-  (setq interprogram-paste-function 'copy-from-osx)
-  (setq interprogram-cut-function 'paste-to-osx))
-
 ;; when opening files, do not try to guess the path from the text under the cursor
 (setq ido-use-filename-at-point nil)
 
