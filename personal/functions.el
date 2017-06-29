af5d5e6999cc67a5e6e56bd6bbd954f1a68d35a9
Iain Beeston <iainbeeston@users.noreply.github.com>
2017-06-29 09:17:12 +0100

    Turn on global commit mode
    Useful when emacs is launched from the command line by git

--- personal/functions.el
+++ personal/functions.el
@@ -1,6 +1,10 @@
 ;; turn on linum automatically
 (global-linum-mode t)
 
+;; turn on git commit mode globally so emacs will use
+;; magit when launched by git
+(global-git-commit-mode)
+
 ;; tell flycheck that web-mode can use eslint
 (with-eval-after-load 'flycheck
   (flycheck-add-mode 'javascript-eslint 'web-mode))
