c3b743511885ccfb14c4adcbafe953ad5ee18087
Iain Beeston <iainbeeston@users.noreply.github.com>
2018-07-26 09:43:47 +0100

    Fixed copy/paste on terminal

--- personal/variables.el
+++ personal/variables.el
@@ -1,3 +1,15 @@
+;; enable copy-paste on the terminal
+(defun copy-from-osx ()
+  (shell-command-to-string "pbpaste"))
+(defun paste-to-osx (text &optional push)
+  (let ((process-connection-type nil))
+    (let ((proc (start-process "pbcopy" "*Messages*" "pbcopy")))
+      (process-send-string proc text)
+      (process-send-eof proc))))
+(when (not window-system)
+  (setq interprogram-paste-function 'copy-from-osx)
+  (setq interprogram-cut-function 'paste-to-osx))
+
 ;; when opening files, do not try to guess the path from the text under the cursor
 (setq ido-use-filename-at-point nil)
 
