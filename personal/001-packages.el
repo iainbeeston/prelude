48c6e46a9bfc723aa8bcf0e3df967df0415ceb9a
Iain Beeston <iainbeeston@users.noreply.github.com>
2019-02-16 09:24:08 +0000

    Alphabetised packages

--- personal/001-packages.el
+++ personal/001-packages.el
@@ -1,6 +1,6 @@
 (prelude-require-package 'ag)
 (prelude-require-package 'chruby)
 (prelude-require-package 'labburn-theme)
-(prelude-require-package 'pcre2el)
 (prelude-require-package 'moody)
+(prelude-require-package 'pcre2el)
 (prelude-require-package 'string-inflection)
